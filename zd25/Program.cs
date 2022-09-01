4f472f2e009acc63c2530a4328fba8364afdcc45
Artem <aneday05@mail.ru>
2022-09-01 23:00:21 +0300

    zd25 completed

--- zd25/Program.cs
+++ zd25/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// 25. Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+
+int DegreeOfNum(int number, int degree)
+{
+    int multi = number;
+    for (int current = 1; current < degree; current ++)
+        multi *= number;
+    
+    return multi;
+}
+Console.WriteLine("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите степень числа: ");
+int deg = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(DegreeOfNum(num, deg));
